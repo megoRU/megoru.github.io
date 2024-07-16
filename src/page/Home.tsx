61dc7f5a66f1a754e8e6e826930e6d340a5ae4ee
megoRU <savin.yura2014@gmail.com>
2024-07-16 17:24:12 +0300

    init

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,18 +1,35 @@
-import { Box } from '@chakra-ui/react';
-import ReactMarkdown from 'react-markdown';
-import ChakraUIRenderer from 'chakra-ui-markdown-renderer';
-import markdownContent from '../blog/index.md';
+import {getBlogs} from "../fetchers.ts";
 
-const Home = () => {
+ function Home() {
     return (
-        <Box>
-            <ReactMarkdown
-                children={markdownContent}
-                components={ChakraUIRenderer()} // Используйте эту строку только при использовании Chakra UI
-                skipHtml // Пропустите это, если не используете Chakra UI
-            />
-        </Box>
-    );
-};
+        <main>
+            {blogs.map((blog, i) => (
+                <article key={i} className="grid grid-cols-4 text-3xl">
+                    <h1>{blog.frontmatter.title}</h1>
+                    <p>{blog.frontmatter.author}</p>
+                    <p>{blog.frontmatter.publishDate}</p>
+                </article>
+            ))}
+        </main>
+    )
+}
+
+//
+// const Home: React.FC = () => (
+//
+//     const blogs = await getBlogs()
+//     return (
+//         <main>
+//             {blogs.map((blog, i) => (
+//                 <article key={i} className="grid grid-cols-4 text-3xl">
+//                     <h1>{blog.frontmatter.title}</h1>
+//                     <p>{blog.frontmatter.author}</p>
+//                     <p>{blog.frontmatter.publishDate}</p>
+//                     <Link href={`/blogs/${blog.slug}`}>Read More</Link>
+//                 </article>
+//             ))}
+//         </main>
+//     )
+// };
 
 export default Home;
